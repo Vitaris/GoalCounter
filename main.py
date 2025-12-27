eb282dd6a7e407bf6280e86a7b0825cc796dce18
Vitaris <53018032+Vitaris@users.noreply.github.com>
2025-12-27 15:30:37 +0100

    Refactor button handling: replace Pin setup with Button class for improved functionality

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import time
-from machine import Pin, Timer
-from matrix_8x8 import matrix_8x8
-from graphics import color_list, square
 import random
 import micropython
+from machine import Pin, Timer
+from time import ticks_ms, ticks_diff
+from button import Button
+from matrix_8x8 import matrix_8x8
+from graphics import COLORS_LIST, square
 
 # Allocate buffer for interrupt exceptions just in case
 micropython.alloc_emergency_exception_buf(100)
@@ -15,12 +16,24 @@ def on_timer(t):
     global frame_tick
     frame_tick = True
 
+def button_callback_01():
+    global speed
+    if speed < 0.2: speed += 0.001
+
+def button_callback_02():
+    global speed
+    if speed > 0.0: speed -= 0.001
+    if speed < 0.0: speed = 0.0
+
+def button_hold_callback():
+    pass
+
 if __name__ == "__main__":
     matrix = matrix_8x8(28, 2, brightness=0.01)
 
     # Simple button setup
-    btn_inc = Pin(1, Pin.IN, Pin.PULL_UP)
-    btn_dec = Pin(14, Pin.IN, Pin.PULL_UP)
+    btn_inc = Button(1, button_callback_01, button_hold_callback)
+    btn_dec = Button(14, button_callback_02, button_hold_callback)
 
     # Setup Timer for 50 FPS (20ms interval)
     # The timer simply sets a flag, keeping the interrupt very short
@@ -32,19 +45,19 @@ if __name__ == "__main__":
 
     # Time for 110 BPM: 60/110 = 0.545s per beat
     # Frames per beat: 0.545s / 0.02s (50FPS) = 27.27 frames
-
+ 
     # Formula: speed = distance / frames = 1.5 / 27.27 = ~0.055
+    global speed
     speed = 0.055
 
     brightness = 0
     dimm = 1.0
     direction = 1 # 1 = increasing, -1 = decreasing
-    color_0 = random.choice(color_list)
-    color_1 = random.choice(color_list)
+    color_0 = random.choice(COLORS_LIST) # type: ignore
+    color_1 = random.choice(COLORS_LIST) # type: ignore
 
     i = 0
-    start_time = time.ticks_ms()
-    last_btn_check = 0
+    start_time = ticks_ms()
     
     while True:
         # Wait for the timer interrupt to set the flag
@@ -54,19 +67,8 @@ if __name__ == "__main__":
             # --- Start of Frame Logic ---
             
             # 1. Poll buttons
-            now = time.ticks_ms()
-            if time.ticks_diff(now, last_btn_check) >= 50:
-                if not btn_inc.value():
-                    if speed < 0.2: speed += 0.001
-                    print(f"Speed increased: {speed:.5f}")
-
-                if not btn_dec.value():
-                    if speed > 0.0: speed -= 0.001
-                    if speed < 0.0: speed = 0.0
-                    print(f"Speed decreased: {speed:.5f}")
-
-                last_btn_check = now
-
+            Button.update_all()
+ 
             # 2. Animation Logic
             brightness += speed * direction
             if brightness < 0:
@@ -74,8 +76,8 @@ if __name__ == "__main__":
                 if speed < 0.02:
                     brightness = 0.0
                 direction = 1
-                color_0 = random.choice(color_list)
-                color_1 = random.choice(color_list)
+                color_0 = random.choice(COLORS_LIST) # type: ignore
+                color_1 = random.choice(COLORS_LIST) # type: ignore
             elif brightness > 1:
                 brightness = 1
                 direction = -1
@@ -85,10 +87,4 @@ if __name__ == "__main__":
             # This happens immediately after the tick, so we have ~10ms before the next one
             matrix.show_symbol(square, color=color_0, show=False, )
             matrix.show_symbol(square, color=color_1, offset=1, show=True)
-            
-            # 4. FPS Counter
-            i += 1
-            if time.ticks_diff(now, start_time) >= 1000:
-                print(f"Speed measurement: {i} frames/sec")
-                start_time = now
-                i = 0
+
